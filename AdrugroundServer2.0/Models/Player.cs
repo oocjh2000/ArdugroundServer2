09c659fa300fed1ad1ee57b63216b7bb1d324574
oocjh2000 <oocjh200@gmail.com>
2019-03-15 22:09:24 +0900

    아두그라운드 다시만들어

---
+++ AdrugroundServer2.0/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace AdrugroundServer2._0.Models
+{
+    public class Player
+    {
+        public Player(Player p)
+        {
+            this.Exp = p.Exp;
+            this.hp = p.hp;
+            this.Name = p.Name;
+        }
+        public string Name;
+        public int Exp;
+        public int hp;
+    }
+}
---
+++ AdrugroundServer2.0/Models/Playground.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AdrugroundServer2._0.Models
+{
+    public class Playground
+    {
+        public Playground(string PlayGroundName, int MaxPlayer)
+        {
+            this.playGroundName = PlayGroundName;
+            this.maxPlayers = MaxPlayer;
+        }
+
+       
+        public string playGroundName;
+        public int playerCount;
+        public Player[] players;
+        private int maxPlayers;
+
+
+    }
+}
